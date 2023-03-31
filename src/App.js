e41ea478d92942f4c0fa99fe9f186c00b864612a
Jota Freitas Jr <jotafreitasjr@gmail.com>
2023-03-31 12:55:55 -0300

    Navigation bar is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Brand, CTA, Navbar } from './components';
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
+import './index.css';
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+        <div className="gradient__bg">
+            <Navbar />
+            <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
   )
 }
